f1063a35b7f3dc50d971b2f67d4800c7fd508672
yadnyesh <yadnyesh.juvekar@gmail.com>
2021-10-12 22:24:22 +0530

    Initial commit

---
+++ src/main/java/io/yadnyesh/reactiveprogramming/ReactiveprogrammingApplication.java
@@ -0,0 +1,13 @@
+package io.yadnyesh.reactiveprogramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveprogrammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveprogrammingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/yadnyesh/reactiveprogramming/ReactiveprogrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package io.yadnyesh.reactiveprogramming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveprogrammingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
