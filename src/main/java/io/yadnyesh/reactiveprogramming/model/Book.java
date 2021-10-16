d34790db86bab2bec5353ac4d4982d38da379ba5
yadnyesh <yadnyesh.juvekar@gmail.com>
2021-10-16 20:23:17 +0530

    Service - Get all books for SpringBoot application

--- src/main/java/io/yadnyesh/reactiveprogramming/model/Book.java
+++ src/main/java/io/yadnyesh/reactiveprogramming/model/Book.java
@@ -1,7 +1,14 @@
 package io.yadnyesh.reactiveprogramming.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Book {
     private BookInfo bookInfo;
     private List<Review> reviewList;
---
+++ src/main/java/io/yadnyesh/reactiveprogramming/service/BookService.java
@@ -0,0 +1,32 @@
+package io.yadnyesh.reactiveprogramming.service;
+
+import io.yadnyesh.reactiveprogramming.model.Book;
+import io.yadnyesh.reactiveprogramming.model.BookInfo;
+import io.yadnyesh.reactiveprogramming.model.Review;
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@Slf4j
+public class BookService {
+
+    private BookInfoService bookInfoService;
+    private ReviewService reviewService;
+
+    public BookService(BookInfoService bookInfoService, ReviewService reviewService) {
+        this.bookInfoService = bookInfoService;
+        this.reviewService = reviewService;
+    }
+
+    public Flux<Book> getBooks() {
+        Flux<BookInfo> bookInfoFlux = bookInfoService.getBooks();
+        return bookInfoFlux
+                .flatMap(bookInfo -> {
+                    Mono<List<Review>> reviews = reviewService.getReviews(bookInfo.getBookId()).collectList();
+                    return reviews
+                            .map(review -> new Book(bookInfo, review));
+                }).log();
+    }
+}
